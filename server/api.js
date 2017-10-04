2d31bd6e6b9f85bae03d3fc7b8d640d1f4698051
mike <mike@localhost.localdomain>
2017-10-03 22:20:54 -0600

    add read json file

--- server/api.js
+++ server/api.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 var getUsers = function(app)
 {
 	app.get('/users', function(req, res){
@@ -7,7 +9,37 @@ var getUsers = function(app)
 
 }
 
+var getData = function(app)
+{
+	app.get('/data', function(req, res)
+	{
+			fs.readFile('./server/data.json',(err,result)=>
+	{
+							
+							if(err)
+							{
+							res.statusCode=404;
+							res.setHeader('Content-Type','application/json');
+							res.send(err);
+
+							}
+							else {
+
+								res.statusCode=200;
+								res.setHeader('Content-Type','application/json');
+								res.send(result);
+							}
+
+
+	});
+
+	});
+
+
+}
+
 module.exports = function(app)
 {
 	getUsers(app);
+	getData(app);
 }
